1981b09e4c6ffc0bed16d9319e5665b53e0bf9ff
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-01-18 05:29:18 -0600

    cors del server configurado y cliente avanzado

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`API escuchando en el puerto ${PORT}`);
+  console.log(`API escuchando en el puerto ${PORT}`);
 });

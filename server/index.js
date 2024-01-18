d194e608909661fe76e749c8e1f0245ba1ca5e71
saul201287 <84940926+saul201287@users.noreply.github.com>
2024-01-18 04:19:00 -0600

    Servidor levantado y configurado

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`API escuchando en el puerto ${PORT}`);
+});
